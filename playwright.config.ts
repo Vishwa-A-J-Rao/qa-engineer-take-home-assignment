1176c9837106effe685fdf306b0ad9d99e605aa5
Vishwa-A-J-Rao <vishwaajrao@gmail.com>
2026-07-05 09:02:50 +0000

    Initialize Playwright TypeScript project

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests',
+
+  fullyParallel: true,
+
+  forbidOnly: !!process.env.CI,
+
+  retries: process.env.CI ? 2 : 0,
+
+  workers: process.env.CI ? 1 : undefined,
+
+  reporter: [['html']],
+
+  use: {
+    baseURL: process.env.BASE_URL,
+    extraHTTPHeaders: {
+      Accept: 'application/json'
+    },
+    testIdAttribute: 'data-test',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    headless: true
+  }
+});
